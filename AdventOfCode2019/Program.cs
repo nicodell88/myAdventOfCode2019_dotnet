a195254b1d39b55e0ae2d2bfd8cf04d847790c7b
Nic O'Dell <nicodell88@gmail.com>
2020-06-26 10:14:02 +1000

    InitialCommit

---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode2019
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
