70c44eb082c19de8fadf3e761710cc50cfec5ee4
Nic O'Dell <nicodell88@gmail.com>
2020-06-27 18:18:45 +1000

    Started Modules

---
+++ AdventOfCode2019/Day1.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq; //inherit the sum method?
+
+
+namespace AdventOfCode2019
+{
+    public class Day1
+    {
+        //Fuel required to launch a given module is based on its mass.
+        //Specifically, to find the fuel required for a module,
+        //take its mass, divide by three, round down, and subtract 2.
+        public void Task1()
+        {
+            List<int> data = Data.GetIntList("../../day1.txt");
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                data[i] = data[i] / 3 - 2;
+            }
+            Console.WriteLine($"Solution to Task 1: {data.Sum()}");
+
+        }
+
+        //Fuel itself requires fuel just like a module - take its mass,
+        //divide by three, round down, and subtract 2. However, that fuel also
+        //requires fuel, and that fuel requires fuel, and so on.Any mass that
+        //would require negative fuel should instead be treated as if it requires
+        //zero fuel; the remaining mass, if any, is instead handled by wishing really hard,
+        //which has no mass and is outside the scope of this calculation.
+        public void Task2()
+        {
+            List<int> data = Data.GetIntList("../../day1.txt");
+            List<int> fuel = new List<int>();
+            foreach (var element in data)
+            {
+                int tmp = element;
+                while (tmp / 3 - 2 > 0)
+                {
+                    tmp = tmp / 3 - 2;
+                    fuel.Add(tmp);
+                }
+            }
+
+            Console.WriteLine($"Solution to Task 2: {fuel.Sum()}");
+        }
+    }
+
+    class Data
+    {
+        internal static List<int> GetIntList(String inp)
+        {
+
+            Console.WriteLine($"Reading from input file {inp}");
+
+            List<int> data = new List<int>();
+
+            using (var reader = new StreamReader(inp))
+            {
+                while (!reader.EndOfStream)
+                {
+                    int line = int.Parse(reader.ReadLine());
+                    data.Add(line);
+                }
+            }
+
+            return data;
+        }
+    }
+}
--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -2,50 +2,16 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+
 namespace AdventOfCode2019
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now);
-
-            List<int> Data = new List<int>();
-
-            using (var reader = new StreamReader("../../day1.txt"))
-            {
-                while (!reader.EndOfStream)
-                {
-                    int line = int.Parse(reader.ReadLine());
-                    Data.Add(line);
-                }
-            }
-
-            //foreach (int element in Data)
-            //{
-            //    Console.WriteLine($"{element}");
-            //}
-            //Console.WriteLine("@@@@@@@@@@@@@@@@@");
-
-            int[] DataArray = Data.ToArray();
-
-            //foreach (var element in DataArray)
-            //{
-            //    Console.WriteLine($"{element}");
-            //}
-            //Console.WriteLine("@@@@@@@@@@@@@@@@@");
-
-            Console.WriteLine(DataArray.Sum());
-
-            //foreach (var element in DataArray)
-            for (int i = 0; i < DataArray.Length; i++)
-            {
-                DataArray[i] = DataArray[i]/3 - 2;
-            }
-
-            Console.WriteLine(DataArray.Sum());
-            //Console.WriteLine(DataArray.Length);
+            Day1 Day1 = new Day1();
+            Day1.Task1();
+            Day1.Task2();
 
         }
     }
